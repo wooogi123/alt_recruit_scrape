3244030174c782d509fac66589106c2fd5d5fc98
Akalee <wooogi123@gmail.com>
2020-05-20 08:51:59 +0900

    first version release

--- mail/mail.go
+++ mail/mail.go
@@ -2,22 +2,39 @@ package mail
 
 import (
 	scrape "../scrape"
+	"bytes"
+	"fmt"
+	"html/template"
 	"log"
 	"net/smtp"
 	"os"
+	"time"
 )
 
-func Send(recruits []scrape.Recruit) {
-	from := os.Getenv("FROM")
-	pass := os.Getenv("PASS")
-	to := os.Getenv("TO")
-	msg := "From: " + from + "\n"
-	msg += "To: " + to + "\n"
-	msg += "Subject: 산업지원 병역일터 공고 목록\n\n"
-
-	for _, recruit := range recruits {
-		msg += recruit.Title + "\n"
+func Parse(recruits []scrape.Recruit) (body string) {
+	fm := template.FuncMap{
+		"formatDate": func(t time.Time) string {
+			return fmt.Sprintf("%d-%02d-%02d", t.Year(), t.Month(), t.Day())
+		},
 	}
+	t := template.Must(template.New("template.html").Funcs(fm).ParseFiles("./mail/template.html"))
+	buf := new(bytes.Buffer)
+	err := t.Execute(buf, recruits)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body = buf.String()
+	return
+}
+
+func Send(recruits []scrape.Recruit) {
+	from := os.Getenv("EMAIL")
+	pass := os.Getenv("PASSWORD")
+	to := os.Getenv("EMAIL")
+	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	subject := "Subject: 산업지원 병역일터 공고 목록\n"
+	body := Parse(recruits)
+	msg := subject + mime + "\n" + body
 
 	err := smtp.SendMail(
 		"smtp.gmail.com:587",
